b117b0fa65f6d147a891a9f1428e71d481665ad1
Sandeepwallkar <sm8562540@gmail.com>
2024-07-24 18:33:23 +0530

    Update m.py
    Signed-off-by: Sandeepwallkar <sm8562540@gmail.com>

--- m.py
+++ m.py
@@ -8,7 +8,7 @@ import os
 from keep_alive import keep_alive
 keep_alive()
 # insert your Telegram bot token here
-bot = telebot.TeleBot('7201884257:AAFvdJKACwpne7uby_4uETjFe6jNfsKg_Bw')
+bot = telebot.TeleBot('BGMI--BOT')
 
 # Admin user IDs
 admin_id = ["5269502580"]
